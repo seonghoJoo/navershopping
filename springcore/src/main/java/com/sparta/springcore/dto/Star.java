5f977f95c3aa349d05007d2080ef610d54494a32
etkl8888 <tjdgh23712021B@@>
2022-01-06 23:02:20 +0900

    ADD 1주차

---
+++ springcore/src/main/java/com/sparta/springcore/dto/Star.java
@@ -0,0 +1,13 @@
+package com.sparta.springcore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Star {
+    String name;
+    int age;
+}
