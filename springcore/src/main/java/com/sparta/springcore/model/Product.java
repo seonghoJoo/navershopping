9cf75b604d2ed1e615137249c91274d41ab2a962
seonghojoo <etkl8888@naver.com>
2022-01-20 22:38:37 +0900

    UPDATE Product 테스트 구현

--- springcore/src/main/java/com/sparta/springcore/model/Product.java
+++ springcore/src/main/java/com/sparta/springcore/model/Product.java
@@ -5,6 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 @Entity // DB 테이블 역할을 합니다.
 @Setter
@@ -38,12 +41,47 @@ public class Product {
 
     // 관심 상품 생성 시 이용합니다.
     public Product(ProductRequestDto requestDto, Long userId) {
-    // 관심상품을 등록한 회원 Id 저장
+        // 입력값 Validation
+        if (userId == null || userId <= 0) {
+            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
+        }
+
+        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
+        }
+
+        if (!isValidUrl(requestDto.getImage())) {
+            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (!isValidUrl(requestDto.getLink())) {
+            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (requestDto.getLprice() <= 0) {
+            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
+        }
+
+        // 관심상품을 등록한 회원 Id 저장
         this.userId = userId;
         this.title = requestDto.getTitle();
         this.image = requestDto.getImage();
         this.link = requestDto.getLink();
         this.lprice = requestDto.getLprice();
         this.myprice = 0;
+    }
+
+    boolean isValidUrl(String url)
+    {
+        try {
+            new URL(url).toURI();
+            return true;
+        }
+        catch (URISyntaxException exception) {
+            return false;
+        }
+        catch (MalformedURLException exception) {
+            return false;
+        }
     }
 }
